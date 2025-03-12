1cf3899fc4d4aa86b36d64d2222eef327f255326
rudraa19 <rudra19206@gmail.com>
2025-03-12 14:29:06 +0530

    feat: enhance content controllers

--- server/src/controller/content/getData.js
+++ server/src/controller/content/getData.js
@@ -2,17 +2,31 @@ import { NAMESPACE } from "../../utils/index.js";
 
 const getData = async (req, res) => {
   const key = req.body.key;
-  let result;
 
-  try {
-    result = await NAMESPACE.get(key);
-  } catch (error) {
-    console.log(error);
+  if (!key) {
+    return res.status(400).json({
+      messge: "Key is required!",
+    });
   }
 
-  res.json({
-    result,
-  });
+  try {
+    const result = await NAMESPACE.get(key);
+
+    if (!result) {
+      return res.status(404).json({
+        messge: "Data not found!",
+      });
+    }
+
+    res.json({
+      result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      messge: "Internal server error",
+    });
+  }
 };
 
 export default getData;
--- server/src/controller/content/storeData.js
+++ server/src/controller/content/storeData.js
@@ -2,29 +2,41 @@ import { NAMESPACE, generateShortId } from "../../utils/index.js";
 
 const storeData = async (req, res) => {
   const data = req.body.data;
-  let key;
 
   if (!data) {
-    res.json({
-      msg: "Data is required!",
+    return res.json({
+      message: "Data is required!",
     });
   }
 
   try {
-    key = generateShortId();
-    const alreadyExist = await NAMESPACE.get(key);
-    if (!alreadyExist) {
-      await NAMESPACE.put(key, data);
-    } else {
-      storeData();
+    let key;
+    let alreadyExist;
+    let attempts = 3;
+
+    do {
+      key = generateShortId();
+      alreadyExist = await NAMESPACE.get(key);
+      attempts--;
+    } while (alreadyExist && attempts > 0);
+
+    if (alreadyExist) {
+      return res
+        .status(500)
+        .json({ message: "Failed to generate a unique key" });
     }
+
+    await NAMESPACE.put(key, data);
+
+    res.json({
+      key,
+    });
   } catch (error) {
     console.log(error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
   }
-
-  res.json({
-    key,
-  });
 };
 
 export default storeData;
