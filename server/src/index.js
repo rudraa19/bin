fd4ae92eae83d656143f0c5f68870a6c0393f5e0
rudraa19 <rudra19206@gmail.com>
2025-03-12 23:34:05 +0530

    fix: cors issue

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import V1Router from "./routes/v1/index.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => res.send("server is running"));
 app.use(V1Router);
