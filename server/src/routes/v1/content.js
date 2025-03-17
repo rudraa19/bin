7ca17fdd93f9018b4fe74ccfd103c8bf4ef7a2ae
rudraa19 <rudra19206@gmail.com>
2025-03-17 22:03:03 +0530

    chore: add rate limit to post route

--- server/src/routes/v1/content.js
+++ server/src/routes/v1/content.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { getAllData, getData, storeData } from "../../controller/index.js";
+import rateLimit from "express-rate-limit";
+
+const postLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 10,
+  message: "Too many requests from this IP, please try again after 10 minutes",
+});
 
 const contentRouter = new Router();
 
-contentRouter.post("/", storeData);
+contentRouter.post("/", postLimiter, storeData);
 contentRouter.get("/", getData);
 contentRouter.get("/all", getAllData);
 
