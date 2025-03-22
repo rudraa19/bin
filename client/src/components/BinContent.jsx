50c300aa0868a8c513095e2a0abdab59a06aeb8f
rudraa19 <rudra19206@gmail.com>
2025-03-22 14:41:03 +0530

    feat: add copy button

--- client/src/components/BinContent.jsx
+++ client/src/components/BinContent.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { client } from '../../config'
 import { Card } from './ui/card';
+import { Button } from './ui/button';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 const BinContent = ({ id }) => {
 
@@ -34,7 +36,12 @@ const BinContent = ({ id }) => {
         <div className='p-[20px]'>
             <h2 className='text-[28px] font-bold'>{title}</h2>
             <br />
-            <Card className="p-[15px]">
+            <Card className="p-[18px] relative">
+                <div className="absolute top-[1rem] right-[1rem]">
+                    <CopyToClipboard text={data} onCopy={() => alert("Copied to clipboard")}>
+                        <Button size="sm" className="p-[5px]">Copy</Button>
+                    </CopyToClipboard>
+                </div>
                 <pre className='w-full break-words whitespace-pre-wrap overflow-auto'>
                     {data}
                 </pre>
