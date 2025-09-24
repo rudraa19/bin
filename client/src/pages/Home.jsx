2e901bfb559aa62a2806eaf2ce8e43ee2f16efd6
rudraa19 <rudra19206@gmail.com>
2025-09-24 12:11:25 +0530

    chore: add footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,9 @@ const Home = () => {
     return (
         <>
             <BinForm />
+            <div className="w-full flex justify-center">
+                Built with ❤️ by <a className="font-medium underline" href="https://github.com/rudraa19/" target="_blank">Rudra</a>
+            </div>
         </>
     )
 }
